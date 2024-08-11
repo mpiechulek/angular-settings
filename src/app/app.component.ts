00b14f8a08065f5140b230e709e2e2a83c382eab
Michal <mpiechulek@gmail.com>
2024-08-11 20:27:04 +0200

    Your commit message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '@env/environment.development';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'front';
+export class AppComponent implements OnInit {
+
+  ngOnInit():void {
+    console.log(environment.apiUrl);
+  }
 }
