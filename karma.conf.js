2ad7afdf336f56a69f462507fdac589503704e74
Michal <mpiechulek@gmail.com>
2024-08-12 11:20:26 +0200

    123

--- karma.conf.js
+++ karma.conf.js
@@ -28,6 +28,19 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage/front'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }],
+      coverageReporter: {
+        dir: require('path').join(__dirname, './coverage/<project-name>'),
+        subdir: '.',
+        reporters: [{ type: 'html' }, { type: 'text-summary' }],
+        check: {
+          global: {
+            statements: 80,
+            branches: 80,
+            functions: 80,
+            lines: 80,
+          },
+        },
+      },
     },
     reporters: ['progress', 'kjhtml'],
     browsers: ['Chrome'],
