871ba72180b3b16a0abad3ee08f1f6d6fb521ba1
Michal <mpiechulek@gmail.com>
2024-08-18 08:55:07 +0200

    dfdffd

--- karma.conf.js
+++ karma.conf.js
@@ -28,17 +28,12 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage/front'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }],
-      coverageReporter: {
-        dir: require('path').join(__dirname, './coverage/<project-name>'),
-        subdir: '.',
-        reporters: [{ type: 'html' }, { type: 'text-summary' }],
-        check: {
-          global: {
-            statements: 80,
-            branches: 80,
-            functions: 80,
-            lines: 80,
-          },
+      check: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80,
         },
       },
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,19 +18,19 @@ describe('AppComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('should create the app', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
 
-  // it('should sum 2 numbers', () => {
-  //   expect(component.onSum(2, 2)).toEqual(4);
-  //   expect(component.onSum(2, 2)).not.toEqual(5);
-  // });
+  it('should sum 2 numbers', () => {
+    expect(component.onSum(2, 2)).toEqual(4);
+    expect(component.onSum(2, 2)).not.toEqual(5);
+  });
 
-  // it('should subtract 2 numbers', () => {
-  //   expect(component.onSubtract(4, 2)).toEqual(2);
-  //   expect(component.onSubtract(2, 2)).not.toEqual(1);
-  // });
+  it('should subtract 2 numbers', () => {
+    expect(component.onSubtract(4, 2)).toEqual(2);
+    expect(component.onSubtract(2, 2)).not.toEqual(1);
+  });
 });
